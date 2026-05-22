cd5a4df5a5cb0816467895b0f8f425ed1be6cf2a
Adam <adamrpdiass@hotmail.com>
2026-05-22 20:03:09 -0300

    add svg icon to Sidebar component

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,10 +1,13 @@
 const SidebarButton = ({ children, variant }) => {
+  const getVariantClasses = () => {
+    return variant != 'unselected'
+      ? 'bg-[#E6F7F8] text-[#00ADB5]'
+      : 'text-[#35383E] bg-white'
+  }
   return (
     <a
-      href=""
-      className={`rounded-lg px-6 py-3 text-[#35383E] ${
-        variant != 'unselected' ? 'bg-[#E6F7F8]' : 'bg-white'
-      }`}
+      href="#"
+      className={`flex items-center gap-2 rounded-lg px-6 py-3 ${getVariantClasses()}`}
     >
       {children}
     </a>
