122bc70d224da221f0c126a646d2df87b344ebe5
Adam <adamrpdiass@hotmail.com>
2026-05-23 19:34:53 -0300

    refactor: add TasksSeparator component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,16 @@
 import Button from './Button'
 import AddIcon from '../assets/icons/add.svg?react'
 import TrashIcon from '../assets/icons/trash.svg?react'
+import SunIcon from '../assets/icons/sun.svg?react'
+import CloudsunIcon from '../assets/icons/cloud-sun.svg?react'
+import MoonIcon from '../assets/icons/moon.svg?react'
+
+import TasksSeparator from './TasksSeparator'
 
 const Tasks = () => {
   return (
     <div className="w-full px-8 py-16">
-      <div className="flex w-full justify-between">
+      <div className="mb-6 flex w-full justify-between">
         <div className="gap-1">
           <span className="text-xs font-semibold text-[#00ADB5]">
             Minhas Tarefas
@@ -20,6 +25,17 @@ const Tasks = () => {
             Nova Tarefa
             <AddIcon />
           </Button>
+        </div>
+      </div>
+      <div className="rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <TasksSeparator title="Manhã" icon={<SunIcon />} />
+        </div>
+        <div className="my-6 space-y-3">
+          <TasksSeparator title="Tarde" icon={<CloudsunIcon />} />
+        </div>
+        <div className="space-y-3">
+          <TasksSeparator title="Noite" icon={<MoonIcon />} />
         </div>
       </div>
     </div>
