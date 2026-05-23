b530bca65500337a7422c3b80d707838c34fcc78
Adam <adamrpdiass@hotmail.com>
2026-05-22 21:39:30 -0300

    feat: add inital Task and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Sidebar from './components/Sidebar'
+import Tasks from './components/Tasks'
 
 function App() {
   return (
-    <div>
+    <div className="flex gap-6">
       <Sidebar />
+      <Tasks />
     </div>
   )
 }
