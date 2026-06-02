852c1048877de940fcd980a11d1f56d39070a449
Adam <adamrpdiass@hotmail.com>
2026-06-02 10:39:00 -0300

    refactor: add prop types TasksSeparator

--- src/components/TasksSeparator.jsx
+++ src/components/TasksSeparator.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const TasksSeparator = ({ title, icon }) => {
   return (
     <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
@@ -5,6 +7,11 @@ const TasksSeparator = ({ title, icon }) => {
       <p className="text-sm text-[#898f97]">{title}</p>
     </div>
   )
+}
+
+TasksSeparator.PropTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.node.isRequired,
 }
 
 export default TasksSeparator
