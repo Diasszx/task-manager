c2b8868c9a38f687490f65f1ce550b71323cfcbb
Adam <adamrpdiass@hotmail.com>
2026-06-13 16:35:36 -0300

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,18 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './App.jsx'
+import Tasks from './components/Tasks.jsx'
+import Home from './pages/home.jsx'
 import TaskDetailsPage from './pages/task-details.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Home />,
+  },
+  {
+    path: '/tasks',
+    element: <Tasks />,
   },
   {
     path: '/task/:taskId',
