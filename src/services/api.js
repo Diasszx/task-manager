cc081d51993592aee7493702d6b1a53acfc28d10
Adam <adamrpdiass@hotmail.com>
2026-06-26 12:09:28 -0300

    debbuging

--- src/services/api.js
+++ src/services/api.js
@@ -12,7 +12,13 @@ export const apiFetch = async (endpoint, options = {}) => {
   try {
     const response = await api({ url: endpoint, ...options })
     return response.data
-  } catch {
-    throw new Error('Erro na requisição')
+  } catch (error) {
+    console.log('API_URL:', API_URL)
+    console.log('Endpoint:', endpoint)
+    console.log('Erro completo:', error)
+    console.log('Status:', error.response?.status)
+    console.log('Data:', error.response?.data)
+
+    throw new Error(error.response?.data?.message || 'Erro na requisição')
   }
 }
